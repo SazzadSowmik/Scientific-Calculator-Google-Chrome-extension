f81b59a1012c59b102c3f7d9413f648d540e2273
SazzadSowmik <sazzad.sowmik@gmail.com>
2023-03-07 02:58:10 +0600

    Added react and webpack with the ext

---
+++ scs-core/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h5>Hello From React App</h5>
+      </header>
+    </div>
+  );
+}
+
+export default App;
