7d4924d450886c055a668a5e773c3644e06f6d0d
SazzadSowmik <sazzad.sowmik@gmail.com>
2023-03-08 03:49:41 +0600

    Environment setup done and Header Also

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../assets/styles/home.css'
+
+const Home = () => {
+  return (
+    <div>
+        <div className="calculator">
+            <div className="top-part"> 
+                <span className="logo">Casio</span>
+                <span className="model">CS-991ES PLUS</span>
+                <span className="svpam">NATURAL- V.P.A.M.</span>
+                <div className="solar"></div>
+                <span className="power">Two way power</span>
+            </div>
+            <div className="screen"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
